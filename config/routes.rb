eeb4c1d321d43a90cd8965326c7a84ce88e51a19
jjsmokovich <JesseSmokovich@yahoo.com>
2019-06-18 15:50:46 -0700

    login form, in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  
   resources :likes
   resources :relationships
   resources :comments
   resources :tweets
   resources :users
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
