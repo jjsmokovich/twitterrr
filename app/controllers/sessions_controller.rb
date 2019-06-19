f7080e127c9e782a91304896c204a3096ec6122a
jjsmokovich <JesseSmokovich@yahoo.com>
2019-06-19 11:07:28 -0700

    created login, and encryption

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
-    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_201729) do
+ActiveRecord::Schema.define(version: 2019_06_19_170024) do
 
   create_table "comments", force: :cascade do |t|
     t.string "post"
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2019_06_18_201729) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
